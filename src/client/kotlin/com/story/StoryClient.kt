a42c846487f3f96901ea1898e7b0cfa0e08a26b8
Mihalic2040 <mihabevza111@gmail.com>
2024-06-27 16:24:03 +0300

    init

---
+++ src/client/kotlin/com/story/StoryClient.kt
@@ -0,0 +1,9 @@
+package com.story
+
+import net.fabricmc.api.ClientModInitializer
+
+object StoryClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
